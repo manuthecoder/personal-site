c6b92fc20659269edd7b80ca9fe8bc363c912183
ManuTheCoder <manusvathgurudath@gmail.com>
2023-02-20 11:09:10 +0000

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,9 +2,9 @@ import "../styles/globals.css";
 import { SessionProvider } from "next-auth/react";
 
 export default function App({
-  Component: any,
+  Component,
   pageProps: { session, ...pageProps },
-}) {
+}: any) {
   return (
     <SessionProvider session={session}>
       <Component {...pageProps} />
